1934176fe39cd95b0bb5be604bbb37dbc5174f39
David Weatherhead <davidweatherhead@davids-macbook-pro.home>
2024-03-04 12:45:06 +0100

    Initial commit of Spring Project from Initializer.

---
+++ src/main/java/com/github/dlweatherhead/todoplanner/TodoPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.github.dlweatherhead.todoplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/dlweatherhead/todoplanner/TodoPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dlweatherhead.todoplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
